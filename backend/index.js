dd88256ef4fe5618f50b6566460c598c05361421
Vincent <salvadorvincentcarl@gmail.com>
2026-05-04 19:02:16 +0800

    first API

--- backend/index.js
+++ backend/index.js
@@ -79,6 +79,61 @@ app.get('/api/v1/outlets/nearby', async (req, res) => {
   }
 })
 
+app.get('/api/v1/outlets/:id/last-visit', async (req, res) => {
+  console.log(`--- Last Visit Request Received: ${new Date().toISOString()} ---`)
+
+  const {id} = req.params
+  if(!id){
+    return res.status(400).json({error:'Outlet ID is required'})
+  }
+
+  try{
+    console.log("Executing last outlet visited")
+    const results = await sql`
+    SELECT
+      rider_id,
+      outlet_id,
+      outcome,
+      arrived_at,
+      departed_at,
+      order_id
+    FROM visits
+    WHERE outlet_id=${id} 
+    ORDER BY arrived_at DESC
+    LIMIT 1
+    `
+    const outlet = await sql`
+    SELECT show_last_visitor, show_last_visit_time
+    FROM outlets_main
+    WHERE id = ${id}
+    `
+    if(results.length===0){
+      return res.json({outlet_id:id,message:"No visits recorded yet"})
+    }
+
+    //Build Response
+    const response = {
+      outlet_id: id,
+      outcome: results[0].outcome,
+      last_visit_date: results[0].departed_at
+    }
+
+    //Conditional Fields
+    if(outlet[0].show_last_visitor){
+      response.rider_id=results[0].rider_id
+    }
+
+    if(outlet[0].show_last_visit_time){
+      response.exact_time=results[0].arrived_at
+    } 
+
+    res.json(response)
+  } catch (err) {
+    console.error('Error:', err.message)
+    res.status(500).json({ error: 'Internal server error' })
+  }
+})
+
 // 6. Start the Server
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
